9375c43bb8a25af868fe562484b56e2e5981954b
Kevin Mooney <kmooney@4b0c11.42berlin.de>
2023-08-15 15:39:49 +0200

    tuesday_afternoon

---
+++ philo_exit.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo_exit.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmooney <kmooney@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/15 12:31:42 by kmooney           #+#    #+#             */
+/*   Updated: 2023/08/15 15:02:22 by kmooney          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philos.h"
+
+void	ft_free_all(t_data *data)
+{
+	ft_destroy_forks(data);
+	if (data->philo)
+		ft_free_philo(data);
+	if (data->forks)
+		ft_free_forks(data);
+	free (data);
+	exit (0);
+}
+
+void	ft_destroy_forks(t_data *data)
+{
+	int	i;
+	char c;
+
+	i = 0;
+	if (data->forks)
+	{
+		while (i < data->num_philo && data->forks[i])
+		{
+			if (pthread_mutex_destroy(&(data->forks[i]->mutex)) != 0)
+			{
+				write(2, "Mutex Destroy fail\n", 19);
+			}
+			i++;
+		}
+	}
+	return ;
+}
+
+void	ft_join_philos(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	if (data->philo)
+	{
+		while(i < data->num_philo && data->philo[i])
+		{
+			if (pthread_join(*(data)->philo[i]->thread, NULL) != 0)
+				write(2, "pthread join fail", 17);
+			i++;
+		}
+	}
+	return ;
+}
+
+void	ft_free_forks(t_data *data)
+{
+	int	i;
+
+	i= 0;
+	while (i < data->num_philo)
+	{
+		free (data->forks[i]);
+		i++;
+	}
+	free (data->forks);
+}
+
+void	ft_free_philo(t_data *data)
+{
+	int	i;
+
+	i= 0;
+	while (i < data->num_philo)
+	{
+		free (data->philo[i]);
+		i++;
+	}
+	free (data->philo);
+}
--- philos.h
+++ philos.h
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kmooney <kmooney@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/29 23:31:07 by lbaron            #+#    #+#             */
-/*   Updated: 2023/08/14 19:26:02 by kmooney          ###   ########.fr       */
+/*   Created: 2023/08/15 12:43:54 by kmooney           #+#    #+#             */
+/*   Updated: 2023/08/15 15:34:28 by kmooney          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,51 @@
 # include <stdlib.h>
 # include <sys/time.h>
 
-//philos_utils1.c
+typedef struct s_philo
+{
+	int				id;
+	int				out;
+	pthread_mutex_t *fork_left;
+	pthread_mutex_t *fork_right;
+	pthread_t		*thread;
+} t_philo;
 
-void print_message(void);
-int	ft_atoi(char *str);
+typedef struct s_forks
+{
+	int				id;
+	pthread_mutex_t	mutex;
+} t_forks;
+
+typedef struct s_data
+{
+	int		num_philo;
+	int		time_to_die;
+	int		time_to_eat;
+	int		time_to_sleep;
+	int		n_to_eat;
+	t_philo	**philo;
+	t_forks	**forks;
+} t_data;
+
+int		main(int argc, char **argv);
+int		ft_atoi(char *str);
+void	print_message(void);
+
+void	ft_input_convert(char **argv, int argc, t_data *data);
+
+void	ft_create_forks(t_data *v);
+
+void	ft_create_philos(t_data *v);
+void	ft_philo_init(t_data *data, int i);
+
+void	ft_thread_init(t_data *data);
+
+void	*ft_routine(void *philo);
+
+void	ft_free_all(t_data *data);
+void	ft_destroy_forks(t_data *data);
+void	ft_join_philos(t_data *data);
+void	ft_free_forks(t_data *data);
+void	ft_free_philo(t_data *data);
 
 #endif //PHILOS_H
